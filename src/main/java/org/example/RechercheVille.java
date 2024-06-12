6cd002c779304ed83bcdb07971b3b5152da4a0cc
François K <francoiska_fr@icloud.com>
2024-06-12 10:42:42 +0200

    Initialisation du projet

---
+++ src/main/java/org/example/RechercheVille.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import jdk.jshell.spi.ExecutionControl;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+public class RechercheVille {
+    private List<String> villes;
+
+    public List<String> rechercher(String mot)  {
+        throw new NotImplementedException();
+    }
+}
